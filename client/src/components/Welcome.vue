f307522024d7d0d9d336c9cd45a74c8c48bbc063
Angel Juarez <angeljuarez729@gmail.com>
2019-07-28 22:44:26 -0400

    building out welcome page for login

---
+++ client/src/components/Welcome.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <h1>Welcome component</h1>
+    <form @submit.prevent="handleSubmit">
+      <label>
+        user_name: 
+        <input v-model="user.user_name" placeholder="user name">
+      </label>
+
+      <label>
+        password: 
+        <input v-model="user.password" placeholder="password" type="password">
+      </label>
+    </form>
+  </div>
+</template>
+
+<script>
+import io from 'socket.io-client';
+
+export default {
+  name: 'Welcome',
+  data() {
+    return {
+      user: {
+        user_name: '',
+        password: '',
+      },
+      message: '',
+      messages: [],
+      socket: io('localhost:3001'),
+    }
+  },
+  methods: {
+    sendMessage(e) {
+      e.preventDefault
+    },
+    handleSubmit() {
+      
+    }
+  },
+}
+</script>
